152308aa9cf67bed1fa04ceb3c36b4e6c73cd5b1
Abdullateef2006 <119931646+Abdullateef2006@users.noreply.github.com>
2023-04-13 20:16:09 +0100

    Add files via upload

---
+++ djangoweb/Blogging/Blog/feeds.py
@@ -0,0 +1,18 @@
+import markdown
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
+from django.urls import reverse_lazy
+from .models import Post
+
+class LatestPostsFeed(Feed):
+    title = 'My Blog'
+    link = reverse_lazy('Blog:Post_List')
+    description = 'New post of my blog'
+    def items(self):
+        return Post.published.all()[:5]
+    def item_title(self, item):
+        return item.title
+    def item_description(self, item):
+        return truncatewords_html(markdown.markdown(item.body),30)        
+    def itempubdate(self, item):
+        return item.published
